5f0ad056d0868fa6b430851698fa9aae18fbf874
Ygor Lazaro <ygor@ygorlazaro.com>
2015-01-17 22:07:40 -0200

    Identação

--- js/regra.js
+++ js/regra.js
@@ -1,28 +1,24 @@
-    $(function() {
+$(function() {
 
-        var JsonBehance = 'https://www.behance.net/v2/users/juansaintclair/projects?api_key=LTGKVyGjYBXqWE39dk69KvWdfI2wRVx0&callback=?';
-        var dados = {} //isso é um objeto
-        
-        $.ajax({
-            url: JsonBehance,
-            type: 'post',
-            dataType: 'json',
-            data: dados,
-            async: true,
-            success: function(retorno) {
-                
-                $.each(retorno.projects, function(index, value) {
-                    var cover_port = value.covers[230];
-                    var name = value.name;
-                    var url_portifolio = value.url;
-                    var class_apply = '.img_capa_' + index;
-                    $('.behance').append('<div class="col-md-5 col-lg-3 wow fadeInUp behance_conteudo" data-wow-duration="3s"> <a href="'+ url_portifolio +'" target="_blank"> <img class="img_capa_' + index + '" src="ok" /> <div class="portfolio_nome"><strong>'+ name +'</strong></div> </a> </div>');
-                    $(class_apply).prop('src', cover_port);  
-                });
-            }
-        });
+    var JsonBehance = 'https://www.behance.net/v2/users/juansaintclair/projects?api_key=LTGKVyGjYBXqWE39dk69KvWdfI2wRVx0&callback=?';
 
-        
+    $.ajax({
+        url: JsonBehance,
+        type: 'get',
+        dataType: 'json',
+        async: true,
+        success: function(retorno) {
+
+            $.each(retorno.projects, function(index, value) {
+                var cover_port = value.covers[230];
+                var name = value.name;
+                var url_portifolio = value.url;
+                var class_apply = '.img_capa_' + index;
+
+				$('.behance').append('<div class="col-md-5 col-lg-3 wow fadeInUp behance_conteudo" data-wow-duration="3s"> <a href="' + url_portifolio + '" target="_blank"> <img class="img_capa_' + index + '" src="ok" /> <div class="portfolio_nome"><strong>' + name + '</strong></div> </a> </div>');
+                $(class_apply).prop('src', cover_port);
+
+            });
+        }
     });
-
-
+});
